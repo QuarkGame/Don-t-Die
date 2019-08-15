35e91440bdd019c3123b253f40c9694c7af8ad80
KevinM <tianzeminkevin@gmail.com>
2019-08-15 23:36:47 +1000

    changed size

--- entities.py
+++ entities.py
@@ -5,3 +5,8 @@ from kivy.uix.widget import Widget
 
 class Tree(Widget):
     pass
+
+
+class Stone(Widget):
+    pass
+
--- player.py
+++ player.py
@@ -1,7 +1,12 @@
 import kivy
 
 from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
 
 
 class Player(Widget):
+    pass
+
+
+class Ground(RelativeLayout):
     pass
