272b90a58693076eeb04562eef07cc582ac47b9b
Edward Ji <jiziao6@gmail.com>
2019-08-15 22:45:32 +1000

    Renewed structure for entities and players and a little demo

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 
+from player import *
+from entities import *
+
 Builder.load_file("player.kv")
 Builder.load_file("entities.kv")
 
