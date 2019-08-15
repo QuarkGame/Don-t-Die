c4427bcba1e1db83c21611aa4b7bab57f19607ff
Edward Ji <jiziao6@gmail.com>
2019-08-15 22:33:01 +1000

    Added structure for entities and players

---
+++ entities.py
@@ -0,0 +1,7 @@
+import kivy
+
+from kivy.uix.widget import Widget
+
+
+class Tree(Widget):
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import kivy
 
 from kivy.app import App
+from kivy.lang import Builder
+
+Builder.load_file("player.kv")
+Builder.load_file("entities.kv")
 
 
 class DontDieApp(App):
