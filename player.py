998cfee96aebcae4d7209384f28f504bd0b4f40e
Edward Ji <jiziao6@gmail.com>
2019-08-16 00:25:47 +1000

    Smoother upwards action

--- player.py
+++ player.py
@@ -12,6 +12,8 @@ class Player(Widget):
         super(Player, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        self._keyboard.bind(on_key_up=self._on_keyboard_up)
+        self.move = None
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -19,14 +21,28 @@ class Player(Widget):
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'w':
-            self.center_y += 5
-            # Clock.schedule_once(lambda dt: (self.center_y + 3), .25)
+            # self.center_y += 5
+            if not self.move:
+                self.move = Clock.schedule_interval(lambda dt: (self.__setattr__("center_y", self.center_y + 1)), .01)
         elif keycode[1] == 's':
             self.center_y -= 5
         elif keycode[1] == 'a':
             self.center_x -= 5
         elif keycode[1] == 'd':
             self.center_x += 5
+        return True
+
+    def _on_keyboard_up(self, keyboard, keycode):
+        if keycode[1] == 'w':
+            if self.move:
+                self.move.cancel()
+                self.move = None
+        elif keycode[1] == 's':
+            pass
+        elif keycode[1] == 'a':
+            pass
+        elif keycode[1] == 'd':
+            pass
         return True
 
 
