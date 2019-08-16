38fc9e1673f8887ce4633b4433b84e39c11a7110
Edward Ji <jiziao6@gmail.com>
2019-08-16 15:15:33 +1000

    Interactive buttons on some entities

--- entities.py
+++ entities.py
@@ -1,6 +1,9 @@
 import kivy
 
+from kivy.clock import Clock
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.vector import Vector
 
 import player
 
@@ -18,13 +21,37 @@ class Transparent(Widget):
     pass
 
 
-class Bush(Entity):
+class Interactive(Widget):
+
+    def __init__(self, **kwargs):
+        Clock.schedule_interval(self.update, 0.01)
+        super(Interactive, self).__init__(**kwargs)
+
+    def update(self, dt):
+        other = player.Player.player
+        dist = Vector(self.center_x, self.center_y).distance(Vector(other.center_x, other.center_y))
+        if dist <= player.interact_limit:
+            if not hasattr(self, "action_btn") or not self.action_btn:
+                self.action_btn = Button(text="E",
+                                         size_hint=(None, None),
+                                         size=(20, 20),
+                                         x=self.x,
+                                         y=self.y)
+                self.action_btn.bind(on_press=lambda _: self.loot())
+                self.add_widget(self.action_btn)
+        else:
+            if hasattr(self, "action_btn") and self.action_btn:
+                self.action_btn = None
+                self.clear_widgets()
+
+
+class Bush(Entity, Interactive):
 
     def loot(self):
         player.Player.player.hunger += 10
 
 
-class FirstAidKit(Entity):
+class FirstAidKit(Entity, Interactive):
 
     def loot(self):
         player.Player.player.health += 10
